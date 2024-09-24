ab6e00425374db00083aeecff9865c48af573c10
Laryssa Savior <97402973+laryssasavior@users.noreply.github.com>
2024-09-23 21:16:18 -0300

    Update script.js

--- js-nutricionista/script.js
+++ js-nutricionista/script.js
@@ -1,8 +1,25 @@
 
 // Função para calcular o IMC
-function calculateIMC() {
+function calculateIMC(event) {
+    event.preventDefault();
+
     const weight = parseFloat(document.getElementById("weight").value);
     const height = parseFloat(document.getElementById("height").value);
-    const imc = weight / (height * height);
-    document.getElementById("imcResult").innerText = `Seu IMC é: ${imc.toFixed(2)}`;
+
+    if (weight > 0 && height > 0) {
+        const imc = weight / (height * height);
+        let resultText;
+
+        if (imc < 18.5) {
+            resultText = "Você está abaixo do peso.";
+        } else if (imc >= 18.5 && imc <= 24.9) {
+            resultText = "Você está com o peso ideal.";
+        } else {
+            resultText = "Você está acima do peso.";
+        }
+
+        document.getElementById("result").textContent = `Seu IMC é ${imc.toFixed(2)}. ${resultText}`;
+    } else {
+        document.getElementById("result").textContent = "Por favor, insira valores válidos.";
+    }
 }
